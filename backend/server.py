2b73553a568ee33e1f4b177ae3abe6534daad810
hank-kuo-cs <hank.cs04@nctu.edu.tw>
2021-01-13 20:24:14 +0800

    update track id

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,5 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, jsonify
+
 
 app = Flask(__name__)
 
@@ -7,6 +7,20 @@ app = Flask(__name__)
 @app.route('/')
 def website():
     return render_template('main.html')
+
+
+@app.route('/api/track/updateID', methods=['POST'])
+def update_id():
+    request_data = request.get_json()
+
+    if 'id' not in request_data:
+        return jsonify({'message': 'data format error, must be {\'id\': \'a\'}'}), 400
+
+    f = open('id.txt', 'w')
+    f.write(request_data['id'])
+    f.close()
+
+    return jsonify({'message': 'success'}), 200
 
 
 if __name__ == '__main__':
